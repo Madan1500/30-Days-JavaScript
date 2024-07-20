30856bb0c630738adaeed2676449d576e30a17e9
Madan Mohan Behera <madanmohan14072002@gmail.com>
2024-07-20 23:19:47 +0530

    Day02

---
+++ Day_2.js
@@ -0,0 +1,45 @@
+let a = 2,b=3;
+// Activity 1
+
+// Task 1 Addition
+console.log(a+b);
+// Task 2 Subtraction
+console.log(a-b)
+// Task 3 Multiply
+console.log(a*b);
+// Task 4 Divide
+console.log(a/b);
+// Task 5 Remainder
+console.log(a%b)
+
+// Activity 2
+
+// Task 6
+a += b
+console.log(a)
+// Task 7
+a -= b
+console.log(b)
+// Task 8
+console.log(a>b)
+console.log(a<b)
+// Task 9
+console.log(a>=b)
+console.log(a<=b)
+// Task 10
+console.log(9=="9")
+console.log(9==="9")
+// Task 11
+console.log((5>6 && 4>=4))
+// Task 12
+console.log((5>6 || 4>=4))
+// Task 13
+console.log(!(5>6 || 4>=4))
+
+let num = 0
+let ans = num>0 ?"positive" :(num === 0)?"Zero":"Negative"
+console.log(ans)
+
+
+
+
